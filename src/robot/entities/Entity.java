b5af2219d29229e1ffda4d141483e4642e59ae07
tuan <levantuan2312@gmail.com>
2016-10-31 03:13:06 +0100

    modified Entity into Abstract Class

--- src/robot/entities/Entity.java
+++ src/robot/entities/Entity.java
@@ -1,23 +1,60 @@
 package robot.entities;
 
 import robot.map.*;
-
+import gui.GUISimulator;
 /**
- * Une entité représente un élément présent sur une case, dans une carte.
+ * Un  entité représente un élément présent sur une case, dans une carte.
  *
- * @see robot.map.Case
- * @see robot.map.Carte
+ * @see robot.map.case
+ * @see robot.map.carte
  */
-public interface Entity {
+public abstract Entity {
+
+    /**
+     * La taille d'une case
+     */
+	public static final TAILLE = 10;
+	
+     private Case cas;
+     private Color col;
+
+    
+    
+    /**
+     * Contructeur d'une nouvelle entité 
+     * 
+     * @param cas la cas sur laquelle elle se trouve
+     * @param col la couleur de l'entité
+     */
+	public Entity(Case cas, Color col){
+		this.cas = cas;
+		this.col = col;
+	}
+
 
 
     /**
-     * Retourne la position de l'entité.
+     * Retourne la case de l'entité.
      * La position d'une entité est la case sur laquelle elle se trouve.
      *
      * @return la position de l'entité
      * @see    robot.map.Case
      */
-    public Case getPosition();
+	public Case getPostion(){
+		return this.cas;
+	}
+	
+	
+    /**
+     * Définit la case de l'entité
+     */
+	public void setPosition(Case cas){
+		this.cas = cas;
+	}
 
+
+    /**
+     * Draw un image represté l'entité sur une case
+     */
+	abstract void draw(GUISimulator gui);
 }
